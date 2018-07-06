0cf9926076488b1f0ebdade4c6e1b04694e59c9c
kellybarber <kelly.james.barber@gmail.com>
2018-07-06 13:21:53 -0400

    Created test for submit

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,6 +1,36 @@
 import React from 'react'
+import { mount } from 'enzyme'
 import CommentBox from 'components/CommentBox'
 
+let wrapped
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox/>)
+})
+
+afterEach(() => {
+  wrapped.unmount()
+})
+
 it('has a text area and a button', () => {
-  
+  expect(wrapped.find('textarea').length).toEqual(1)
+  expect(wrapped.find('button').length).toEqual(1)
+})
+
+it('has a text area that users can type in', () => {
+  const comment = 'new comment'
+
+  wrapped.find('textarea').simulate('change', {
+    target: { value: comment }
+  })
+  wrapped.update()
+
+  expect(wrapped.find('textarea').prop('value')).toEqual(comment)
+})
+
+it('clears input after submit', () => {
+  wrapped.find('form').simulate('submit')
+  wrapped.update()
+
+  expect(wrapped.find('textarea').prop('value')).toEqual('')
 })
