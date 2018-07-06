605c7d7bdbad57f740cb734270789c449cec0cc2
kellybarber <kelly.james.barber@gmail.com>
2018-07-06 16:25:31 -0400

    Added root component to set up store to both index and tests

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import * as actions from 'actions'
 
 class CommentBox extends Component {
   state = { comment: '' }
@@ -10,6 +12,11 @@ class CommentBox extends Component {
 
   handleSubmit = event => {
     event.preventDefault()
+    const { comment } = this.state
+    const { saveComment } = this.props
+
+    saveComment(comment)
+
     this.setState({ comment: '' })
   }
 
@@ -28,4 +35,4 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox
+export default connect(null, actions)(CommentBox)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducers from 'reducers'
-
+import Root from 'Root'
 import App from 'components/App'
 
-const store = createStore( reducers, {} )
-
-const AppRoot = (
-  <Provider store={store}>
-    <App/>
-  </Provider>
-)
-
-ReactDOM.render(AppRoot, document.querySelector('#root'))
+ReactDOM.render(<Root><App/></Root>, document.querySelector('#root'))
